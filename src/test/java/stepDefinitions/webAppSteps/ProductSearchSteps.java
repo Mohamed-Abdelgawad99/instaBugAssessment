5cb6282cd49ddb11fec513b989406d96686e21f0
Mohamed Aly <m.e.abdelgawad@gmail.com>
2025-03-22 05:47:51 +0200

    testcases logging

--- src/test/java/stepDefinitions/webAppSteps/ProductSearchSteps.java
+++ src/test/java/stepDefinitions/webAppSteps/ProductSearchSteps.java
@@ -1,8 +1,10 @@
 package stepDefinitions.webAppSteps;
 
+import io.cucumber.java.Scenario;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
 import org.junit.Assert;
+import io.cucumber.java.Before;
 import org.openqa.selenium.WebDriver;
 import pages.webAppPages.SearchResultsPage;
 import utils.WebDriverFactory;
@@ -10,19 +12,28 @@ import utils.WebDriverFactory;
 public class ProductSearchSteps {
     private final WebDriver driver = WebDriverFactory.getDriver();
     private final SearchResultsPage searchResultsPage = new SearchResultsPage(driver);
+    private Scenario scenario;
 
+    @Before
+    public void beforeScenario(Scenario scenario) {
+        this.scenario = scenario;
+    }
 
     @Then("search results page loads")
     public void searchResultsPageLoads() {
+        scenario.log("Checking if search results page is loaded..");
         Assert.assertTrue(searchResultsPage.loadingSearchResultsPage());
+        scenario.log("Search results page loaded successfully");
     }
 
     @And("check search result page has at least {int} results")
     public void searchResultPageHasResults(int numberOfSearchResults) {
+        scenario.log("Validating number of search results");
         int actualSearchResults = searchResultsPage.validateNumberOfSearchResults();
         boolean searchResultsComparison = actualSearchResults >= numberOfSearchResults;
         Assert.assertTrue("Found "+ actualSearchResults + " results and required at least " +
                         numberOfSearchResults, searchResultsComparison);
+        scenario.log();
 
     }
 
